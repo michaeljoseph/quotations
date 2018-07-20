3a4c0e92ca0f973dfb989b00817a1182ce767e9c
Michael Joseph <michaeljoseph+github@gmail.com>
2018-07-20 17:48:01 +0200

    Use jinja for replacement

--- quotes.py
+++ quotes.py
@@ -1,31 +1,45 @@
+"""
+TODO:
+- [] cli: quotes.py <path-to-template> <path-to-data>
+"""
+import uuid
+
 from docx import Document
+from jinja2 import Template
 
-FILENAME='SLB Consulting Proposal-Template.docx'
-TEMPLATE_CLIENT='SLB Consulting'
-CLIENT='Michael Joseph'
+def template_render(template, replacements):
+    return Template(template).render(replacements)
 
-doc = Document(FILENAME)
+def main(template_document, replacements):
+    doc = Document(template_document)
 
-# FIXME: header / footer
-# for section in doc.sections:
-#     print(dir(section))
-#     print(section.header)
+    # FIXME: header / footer
+    # for section in doc.sections:
+    #     print(dir(section))
+    #     print(section.header)
 
-print(dir(doc.paragraphs[0].runs[0]))
-for paragraph in doc.paragraphs:
-    for run in paragraph.runs:
-        if TEMPLATE_CLIENT in run.text:
-            run.text = run.text.replace(TEMPLATE_CLIENT, CLIENT)
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            run.text = template_render(run.text, replacements)
 
-    if 'SLB Consulting' in paragraph.text:
-        paragraph.text = paragraph.text.replace('SLB Consulting', CLIENT)
+        paragraph.text = template_render(paragraph.text, replacements)
 
-for table in doc.tables:
-    for row in table.rows:
-        for cell in row.cells:
-            for paragraph in cell.paragraphs:
-                if TEMPLATE_CLIENT in paragraph.text:
-                    paragraph.text = paragraph.text.replace(TEMPLATE_CLIENT, CLIENT)
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    paragraph.text = template_render(paragraph.text, replacements)
 
-doc.save('{}-proposal.docx'.format(CLIENT))
+    output_filename = 'generated-proposal-{}.docx'.format(uuid.uuid4().hex)
+    print(output_filename)
+    doc.save(output_filename)
 
+if __name__ == '__main__':
+    TEMPLATE_FILENAME='Proposal-Template.docx'
+    replacements = {
+        'client_name': 'Michael Joseph',
+        'system_size': '27.2 kWp',
+        'annual_production': '43 683.2 kWh/a',
+        'cost': 'R 476 292.54',
+    }
+    main(TEMPLATE_FILENAME, replacements)
