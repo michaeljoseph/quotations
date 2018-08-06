db37ef9c8f4074e75a1ee13e06686adbbe5f3928
Michael Joseph <michaeljoseph+github@gmail.com>
2018-08-07 00:25:45 +0200

    Separate framework from actual code

--- quotations/api.py
+++ quotations/api.py
@@ -19,49 +19,49 @@ def allowed_file(upload, extensions):
     )
 
 
+def generate_quote(template, replacements):
+    if allowed_file(template, ALLOWED_TEMPLATE_EXTENSIONS):
+        if allowed_file(replacements, ALLOWED_REPLACEMENTS_EXTENSIONS):
+
+            cfp = io.StringIO()
+            cfp.write(
+                replacements.read().decode('utf-8')
+            )
+            cfp.seek(0)
+
+            replacements = {
+                row['variable']: row['value']
+                for row in csv.DictReader(cfp)
+            }
+            document = generate_quote_from_template(
+                Document(template), replacements
+            )
+
+            fp = io.BytesIO()
+            document.save(fp)
+            fp.seek(0)
+            return send_file(
+                fp,
+                as_attachment=True,
+                attachment_filename='generated-proposal-{}.docx'.format(
+                    uuid.uuid4().hex
+                ),
+            )
+    return None
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     return redirect(request.url)
-
+        error_message = None
         template = request.files['template']
         replacements = request.files['replacements']
 
         # if user does not select file, browser also
         # submit an empty part without filename
         if template.filename == '' or replacements.filename == '':
-            print('No selected files')
-            return redirect(request.url)
-
-        if allowed_file(template, ALLOWED_TEMPLATE_EXTENSIONS):
-            if allowed_file(replacements, ALLOWED_REPLACEMENTS_EXTENSIONS):
-
-                cfp = io.StringIO()
-                cfp.write(
-                    replacements.read().decode('utf-8')
-                )
-                cfp.seek(0)
-
-                replacements = {
-                    row['variable']: row['value']
-                    for row in csv.DictReader(cfp)
-                }
-                document = generate_quote_from_template(
-                    Document(template), replacements
-                )
-
-                fp = io.BytesIO()
-                document.save(fp)
-                fp.seek(0)
-                return send_file(
-                    fp,
-                    as_attachment=True,
-                    attachment_filename='generated-proposal-{}.docx'.format(
-                        uuid.uuid4().hex
-                    ),
-                )
-
+            return {
+                'error': 'No selected files'
+            }
+        return generate_quote(template, replacements)
+    
     return render_template('index.html')
